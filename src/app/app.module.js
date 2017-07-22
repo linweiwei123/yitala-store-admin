6c4dc803bcc3afa3eba97457c86b67b0269105ef
陈 志标 <1021782372@qq.com>
2017-07-22 23:25:04 +0800

    bug修复

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,10 +1,4 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
@@ -41,43 +35,45 @@ var AppModule = (function () {
     }
     return AppModule;
 }());
-AppModule = __decorate([
-    core_1.NgModule({
-        imports: [
-            ng_bootstrap_1.NgbModule.forRoot(),
-            platform_browser_1.BrowserModule,
-            http_1.HttpModule,
-            forms_1.FormsModule,
-            forms_1.ReactiveFormsModule,
-            app_routing_module_1.AppRoutingModule,
-            share_module_1.ShareModule,
-            angular2_highcharts_1.ChartModule,
-            product_module_1.ProductModule,
-            angular2_notifications_1.SimpleNotificationsModule.forRoot(),
-            animations_1.BrowserAnimationsModule
-        ],
-        declarations: [
-            app_component_1.AppComponent,
-            sidebar_component_1.SidebarComponent,
-            login_component_1.LoginComponent,
-            dashboard_component_1.DashboardComponent,
-            product_edit_component_1.ProductEditComponent
-        ],
-        providers: [
-            auth_guard_component_1.AuthGuard,
-            authentication_service_1.AuthenticationService,
-            no_authed_guard_component_1.NoAuthedGuard,
-            upload_service_1.UploadService,
-            product_service_1.ProductService,
-            {
-                provide: HighchartsService_1.HighchartsStatic,
-                useFactory: highchartsFactory
-            },
-            state_service_1.StateService
-        ],
-        entryComponents: [alert_component_1.AlertComponent],
-        bootstrap: [app_component_1.AppComponent]
-    })
-], AppModule);
+AppModule.decorators = [
+    { type: core_1.NgModule, args: [{
+                imports: [
+                    ng_bootstrap_1.NgbModule.forRoot(),
+                    platform_browser_1.BrowserModule,
+                    http_1.HttpModule,
+                    forms_1.FormsModule,
+                    forms_1.ReactiveFormsModule,
+                    app_routing_module_1.AppRoutingModule,
+                    share_module_1.ShareModule,
+                    angular2_highcharts_1.ChartModule,
+                    product_module_1.ProductModule,
+                    angular2_notifications_1.SimpleNotificationsModule.forRoot(),
+                    animations_1.BrowserAnimationsModule
+                ],
+                declarations: [
+                    app_component_1.AppComponent,
+                    sidebar_component_1.SidebarComponent,
+                    login_component_1.LoginComponent,
+                    dashboard_component_1.DashboardComponent,
+                    product_edit_component_1.ProductEditComponent
+                ],
+                providers: [
+                    auth_guard_component_1.AuthGuard,
+                    authentication_service_1.AuthenticationService,
+                    no_authed_guard_component_1.NoAuthedGuard,
+                    upload_service_1.UploadService,
+                    product_service_1.ProductService,
+                    {
+                        provide: HighchartsService_1.HighchartsStatic,
+                        useFactory: highchartsFactory
+                    },
+                    state_service_1.StateService
+                ],
+                entryComponents: [alert_component_1.AlertComponent],
+                bootstrap: [app_component_1.AppComponent]
+            },] },
+];
+/** @nocollapse */
+AppModule.ctorParameters = function () { return []; };
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
--- src/app/product/list/product-list.component.js
+++ src/app/product/list/product-list.component.js
@@ -2,15 +2,6 @@
 /**
  * Created by Linweiwei on 2017/1/12.
  */
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var product_service_1 = require("../../share/service/product.service");
@@ -154,18 +145,21 @@ var ProductListComponent = (function () {
     };
     return ProductListComponent;
 }());
-ProductListComponent = __decorate([
-    core_1.Component({
-        selector: 'product-list',
-        templateUrl: 'product-list.component.html',
-        styleUrls: ['product-list.component.css']
-    }),
-    __metadata("design:paramtypes", [product_service_1.ProductService,
-        forms_1.FormBuilder,
-        router_1.Router,
-        router_1.ActivatedRoute,
-        state_service_1.StateService,
-        angular2_notifications_1.NotificationsService])
-], ProductListComponent);
+ProductListComponent.decorators = [
+    { type: core_1.Component, args: [{
+                selector: 'product-list',
+                templateUrl: 'product-list.component.html',
+                styleUrls: ['product-list.component.css']
+            },] },
+];
+/** @nocollapse */
+ProductListComponent.ctorParameters = function () { return [
+    { type: product_service_1.ProductService, },
+    { type: forms_1.FormBuilder, },
+    { type: router_1.Router, },
+    { type: router_1.ActivatedRoute, },
+    { type: state_service_1.StateService, },
+    { type: angular2_notifications_1.NotificationsService, },
+]; };
 exports.ProductListComponent = ProductListComponent;
 //# sourceMappingURL=product-list.component.js.map
